25540c6a1d2cd4d4ebc445e860746f868474fb93
Wilfredo Joel Solano <87105478+Wlly1618@users.noreply.github.com>
2023-08-21 22:03:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,104 @@
-import openpyxl
-import csv
+import os
+from tkinter import filedialog
+import xlrd
 import calendar
+import csv
+import openpyxl
 
 
-def get_data_fo_f2(worksheet, max_row):
-    data_fo_f2 = []
-    for row in worksheet.iter_rows(min_row=10, max_row=max_row + 9, min_col=2, max_col=25, values_only=True):
-        data_fo_f2.append(row)
+def read_folder():
+    route_folder = filedialog.askdirectory()
+    file_names = os.listdir(route_folder)
+    file_paths = [os.path.join(route_folder, name_file) for name_file in file_names if
+                  os.path.isfile(os.path.join(route_folder, name_file))]
 
-    return data_fo_f2
-
-
-def get_data_times():
-    data_times = []
-    for i in range(24):
-        data_times.append(i)
-
-    return data_times
+    return file_paths
 
 
-def write_data(file_out, header, year, month, data_times, data_fo_f2, data_fo_f2_correct):
-    with open(file_out, 'w', newline='') as archivo_csv:
+def write_data(year, month, data_fo_f2, data_fo_f2_correct):
+    data_time = [i for i in range(24)]
+    header = ['year', 'month', 'day', 'time', 'foF2', 'foF2_']
+    file = str(year) + " " + str(month) + ".csv"
+
+    with open(file, 'w', newline='') as archivo_csv:
         writer = csv.writer(archivo_csv)
         writer.writerow(header)
         i = 1
         for row_fo_f2, row_fo_f2_c in zip(data_fo_f2, data_fo_f2_correct):
-            for fo_f2, fo_f2_c, time in zip(row_fo_f2, row_fo_f2_c, data_times):
-                # print("day: ", i, "\t time: ", time, "\tfo_f2: ", fo_f2, "\tfo_f2_correct: ", fo_f2_c)
+            for fo_f2, fo_f2_c, time in zip(row_fo_f2, row_fo_f2_c, data_time):
+                print("day: ", i, "\t time: ", time, "\tfo_f2: ", fo_f2, "\tfo_f2_correct: ", fo_f2_c)
                 writer.writerow([year, month, i, time, fo_f2, fo_f2_c])
             i += 1
 
         archivo_csv.close()
 
-    print("file created :)")
+
+def get_data_fo_f2_xls(worksheet, amo_days: int):
+    data = []
+    beg_row = 9
+    end_row = amo_days + 9
+    beg_col = 1
+    end_col = 25
+    for row in range(beg_row, end_row):
+        temp = worksheet.row_values(row, beg_col, end_col)
+        data.append(temp)
+
+    return data
 
 
-def check_data(col_data_fo_f2, col_data_fo_f2_correct):
-    for r1, r2 in zip(col_data_fo_f2, col_data_fo_f2_correct):
-        if r1 != r2:
-            return False
+def get_data_fo_f2_xlsx(worksheet, amo_days: int):
+    data_fo_f2 = []
+    for row in worksheet.iter_rows(min_row=10, max_row=(amo_days + 9), min_col=2, max_col=25, values_only=True):
+        data_fo_f2.append(row)
 
-    return True
+    return data_fo_f2
 
 
-def main():
-    workbook = openpyxl.load_workbook('./file.xlsx')
-    sheet = workbook.sheetnames[0]
-    worksheet = workbook[sheet]
+def read_and_write_xlsx(route: str):
+    workbook = openpyxl.load_workbook(route)
+    worksheet_fof2 = workbook[workbook.sheetnames[0]]
+    worksheet_fof2_correct = workbook[workbook.sheetnames[2]]
 
-    data_times = get_data_times()
-    month = worksheet['F6'].value
-    year = worksheet['H6'].value
+    month: int = int(worksheet_fof2['F6'].value)
+    year: int = int(worksheet_fof2['H6'].value)
     amo_days = calendar.monthrange(year=year, month=month)
-    data_fo_f2 = get_data_fo_f2(worksheet, amo_days[1])
-    col_data_fo_f2 = []
-    for row in worksheet.iter_rows(min_row=10, max_row=amo_days[1] + 9, min_col=1, max_col=1, values_only=True):
-        col_data_fo_f2.append(row)
 
-    sheet = workbook.sheetnames[2]
-    worksheet = workbook[sheet]
-    data_fo_f2_correct = get_data_fo_f2(worksheet, amo_days[1])
-    col_data_fo_f2_correct = []
-    for row in worksheet.iter_rows(min_row=10, max_row=amo_days[1] + 9, min_col=1, max_col=1, values_only=True):
-        col_data_fo_f2_correct.append(row)
+    data_fo_f2 = get_data_fo_f2_xlsx(worksheet_fof2, amo_days[1])
+    data_fo_f2_correct = get_data_fo_f2_xlsx(worksheet_fof2_correct, amo_days[1])
 
-    header = ['year', 'month', 'day', 'time', 'foF2', 'foF2_']
-    file = "output.csv"
-
-    flag = check_data(col_data_fo_f2, col_data_fo_f2_correct)
-
-    if flag == 1:
-        write_data(file, header, year, month, data_times, data_fo_f2, data_fo_f2_correct)
-    else:
-        print("data bad")
-
+    write_data(year, month, data_fo_f2, data_fo_f2_correct)
     workbook.close()
 
 
-if __name__ == '__main__':
+def read_and_write_xls(route: str):
+    workbook = xlrd.open_workbook(route)
+    worksheets_names = workbook.sheet_names()
+
+    worksheet_fof2 = workbook.sheet_by_name(worksheets_names[0])
+    worksheet_fof2_correct = workbook.sheet_by_name(worksheets_names[2])
+
+    month: int = int(worksheet_fof2.cell_value(5, 5))
+    year: int = int(worksheet_fof2.cell_value(5, 7))
+    amo_days = calendar.monthrange(year=year, month=month)
+
+    data_fo_f2 = get_data_fo_f2_xls(worksheet_fof2, amo_days[1])
+    data_fo_f2_correct = get_data_fo_f2_xls(worksheet_fof2_correct, amo_days[1])
+
+    write_data(year, month, data_fo_f2, data_fo_f2_correct)
+
+
+def main():
+    list_routes = read_folder()
+    for route in list_routes:
+        ext = os.path.splitext(route)[1]
+        print(ext)
+        if '.xlsx' == ext:
+            read_and_write_xlsx(route)
+        elif '.xls' == ext:
+            read_and_write_xls(route)
+        else:
+            print('No excel file')
+
+
+if __name__ == "__main__":
     main()
