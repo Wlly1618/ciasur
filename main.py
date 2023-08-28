326729e946299d787a23c87bd3177a04a632f7b0
Wilfredo Joel Solano <87105478+Wlly1618@users.noreply.github.com>
2023-08-27 22:04:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def read_folder():
     return file_paths
 
 
-def write_data(year, month, data_fo_f2, data_fo_f2_correct):
+def write_data(year, month, data_fo_f2, data_fo_f2_correct, folder: str):
     data_time = [i for i in range(24)]
     header = ['year', 'month', 'day', 'time', 'foF2', 'foF2_']
-    file = str(year) + " " + str(month) + ".csv"
+    file = folder + "/" + str(year) + " " + str(month) + ".csv"
 
     with open(file, 'w', newline='') as archivo_csv:
         writer = csv.writer(archivo_csv)
@@ -26,7 +26,7 @@ def write_data(year, month, data_fo_f2, data_fo_f2_correct):
         i = 1
         for row_fo_f2, row_fo_f2_c in zip(data_fo_f2, data_fo_f2_correct):
             for fo_f2, fo_f2_c, time in zip(row_fo_f2, row_fo_f2_c, data_time):
-                print("day: ", i, "\t time: ", time, "\tfo_f2: ", fo_f2, "\tfo_f2_correct: ", fo_f2_c)
+                # print("day: ", i, "\t time: ", time, "\tfo_f2: ", fo_f2, "\tfo_f2_correct: ", fo_f2_c)
                 writer.writerow([year, month, i, time, fo_f2, fo_f2_c])
             i += 1
 
@@ -54,50 +54,78 @@ def get_data_fo_f2_xlsx(worksheet, amo_days: int):
     return data_fo_f2
 
 
-def read_and_write_xlsx(route: str):
-    workbook = openpyxl.load_workbook(route)
-    worksheet_fof2 = workbook[workbook.sheetnames[0]]
-    worksheet_fof2_correct = workbook[workbook.sheetnames[2]]
+def read_and_write_xlsx(route: str, folder: str):
+    try:
+        workbook = openpyxl.load_workbook(route)
+        worksheet_fof2 = workbook[workbook.sheetnames[0]]
+        worksheet_fof2_correct = workbook[workbook.sheetnames[2]]
 
-    month: int = int(worksheet_fof2['F6'].value)
-    year: int = int(worksheet_fof2['H6'].value)
-    amo_days = calendar.monthrange(year=year, month=month)
+        month: int = int(worksheet_fof2['F6'].value)
+        year: int = int(worksheet_fof2['H6'].value)
+        amo_days = calendar.monthrange(year=year, month=month)
 
-    data_fo_f2 = get_data_fo_f2_xlsx(worksheet_fof2, amo_days[1])
-    data_fo_f2_correct = get_data_fo_f2_xlsx(worksheet_fof2_correct, amo_days[1])
+        data_fo_f2 = get_data_fo_f2_xlsx(worksheet_fof2, amo_days[1])
+        data_fo_f2_correct = get_data_fo_f2_xlsx(worksheet_fof2_correct, amo_days[1])
 
-    write_data(year, month, data_fo_f2, data_fo_f2_correct)
-    workbook.close()
+        write_data(year, month, data_fo_f2, data_fo_f2_correct, folder)
+        workbook.close()
+    except IndexError:
+        print("Index out range")
+        bad_index.append(route)
+    except Exception as e:
+        bad_files.append(route)
+        print(f"Other error: {e}")
 
 
-def read_and_write_xls(route: str):
-    workbook = xlrd.open_workbook(route)
-    worksheets_names = workbook.sheet_names()
+def read_and_write_xls(route: str, folder: str):
+    try:
+        workbook = xlrd.open_workbook(route)
+        worksheets_names = workbook.sheet_names()
 
-    worksheet_fof2 = workbook.sheet_by_name(worksheets_names[0])
-    worksheet_fof2_correct = workbook.sheet_by_name(worksheets_names[2])
+        worksheet_fof2 = workbook.sheet_by_name(worksheets_names[0])
+        worksheet_fof2_correct = workbook.sheet_by_name(worksheets_names[2])
 
-    month: int = int(worksheet_fof2.cell_value(5, 5))
-    year: int = int(worksheet_fof2.cell_value(5, 7))
-    amo_days = calendar.monthrange(year=year, month=month)
+        month: int = int(worksheet_fof2.cell_value(5, 5))
+        year: int = int(worksheet_fof2.cell_value(5, 7))
+        amo_days = calendar.monthrange(year=year, month=month)
 
-    data_fo_f2 = get_data_fo_f2_xls(worksheet_fof2, amo_days[1])
-    data_fo_f2_correct = get_data_fo_f2_xls(worksheet_fof2_correct, amo_days[1])
+        data_fo_f2 = get_data_fo_f2_xls(worksheet_fof2, amo_days[1])
+        data_fo_f2_correct = get_data_fo_f2_xls(worksheet_fof2_correct, amo_days[1])
 
-    write_data(year, month, data_fo_f2, data_fo_f2_correct)
+        write_data(year, month, data_fo_f2, data_fo_f2_correct, folder)
+    except IndexError:
+        print("Index out range")
+        bad_index.append(route)
+    except Exception as e:
+        print(f"Other error: {e}")
+        bad_files.append(route)
+
+
+bad_files = []
+bad_index = []
 
 
 def main():
     list_routes = read_folder()
+    route_folder = filedialog.askdirectory()
+    print(route_folder)
     for route in list_routes:
         ext = os.path.splitext(route)[1]
-        print(ext)
+        print(route)
         if '.xlsx' == ext:
-            read_and_write_xlsx(route)
+            read_and_write_xlsx(route, route_folder)
         elif '.xls' == ext:
-            read_and_write_xls(route)
+            read_and_write_xls(route, route_folder)
         else:
             print('No excel file')
+
+    print(f"Amount Bad Files: {len(bad_files)}")
+    for file in bad_files:
+        print(f"\tFile: {file}")
+
+    print(f"Amount Bad Index: {len(bad_index)}")
+    for file in bad_index:
+        print(f"\tFile: {file}")
 
 
 if __name__ == "__main__":
